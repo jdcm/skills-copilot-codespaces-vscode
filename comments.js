68ee44861df0bf7f28fa297b9e0579759d72929a
jdcm <james.mckeown@acutest.com>
2023-07-20 09:31:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,75 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
+
+// Create a web server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+// Get all comments for a post
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create a comment for a post
+app.post('/posts/:id/comments', async (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content, status: 'pending' });
+    commentsByPostId[req.params.id] = comments;
+
+    // Emit event to event-bus
+    await axios.post('http://event-bus-srv:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    });
+
+    res.status(201).send(comments);
+});
+
+// Receive events from event-bus
+app.post('/events', async (req, res) => {
+    console.log('Event received: ', req.body.type);
+
+    const { type, data } = req.body;
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        // Emit event to event-bus
+        await axios.post('http://event-bus-srv:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        });
+    }
+
+    res.send({});
+});
+
+// Start listening
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
+});
